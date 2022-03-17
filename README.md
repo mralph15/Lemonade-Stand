8fb7e5901b4e20486479d41acf085f499437c281
Matthew Ralph <54553694+mralph15@users.noreply.github.com>
2022-03-17 16:26:31 -0400

    Moved logo to main folder + dataset demographics

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<img src="https://github.com/mralph15/Lemonade-Stand/blob/main/img/lemonde-stand-logo.png?raw=true" width="128" align="right" />
+<img src="https://github.com/mralph15/Lemonade-Stand/blob/main/lemonde-stand-logo.png?raw=true" width="128" align="right" />
 
 # Lemonade-Stand
 Creation of a replicable report on the sales of a fictional lemonade stand with randomly generated data.
--- dataset_creation.R
+++ dataset_creation.R
@@ -30,5 +30,70 @@ month_long <- month %>% uncount(cars)
 month_long <- left_join(month_long, month %>% select(day, cars), by = "day") %>%
   rename("total_cars" = "cars")
 
-#car characteristics
+#did car stop?
+for(i in 1:nrow(month_long)){
+  month_long$car_stopped[i] = as.numeric(rbernoulli(1, 0.5))
+}
+
+#color of car
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$car_color[i] = sample(c("black", "white", "red", "blue", "other"), size = 1,
+                                     prob = c(.3, .3, .1, .2, .1))
+  }
+  else{
+    month_long$car_color[i] = NA
+  }
+}
+
+#driver preferences
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$prefs[i] = sample(c("hungry", "thirsty", "both"), size = 1,
+                                 prob = c(0.2, 0.5, 0.3))
+  }
+  else{
+    month_long$prefs[i] = NA
+  }
+}
+
+#driver age
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$age[i] = sample(16:80, 1)
+  }
+  else{
+    month_long$age[i] = NA
+  }
+}
+
+#driver gender
+male_prob = c()
+for(i in 1:28){
+  male_prob[i] = runif(1, 0, 1)
+}
+
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$gender[i] = sample(c("M", "F"), size = 1,
+                                  prob = c(male_prob[month_long$day[i]], 1-male_prob[month_long$day[i]]))
+  }
+  else{
+    month_long$gender[i] = NA
+  }
+}
+
+#driver coupon
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$coupon[i] = rbernoulli(1, 0.2)
+  }
+  else{
+    month_long$coupon[i] = NA
+  }
+}
+
+
+
+
 
