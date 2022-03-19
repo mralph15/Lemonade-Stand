97a35bf7b1d222a027739fa80a159ead0479df08
Matthew Ralph <54553694+mralph15@users.noreply.github.com>
2022-03-19 12:53:58 -0400

    Finishing test dataset creation

--- dataset_creation.R
+++ dataset_creation.R
@@ -39,21 +39,10 @@ for(i in 1:nrow(month_long)){
 for(i in 1:nrow(month_long)){
   if(month_long$car_stopped[i] == 1){
     month_long$car_color[i] = sample(c("black", "white", "red", "blue", "other"), size = 1,
-                                     prob = c(.3, .3, .1, .2, .1))
+                                     prob = c(0.3, 0.3, 0.1, 0.2, 0.1))
   }
   else{
     month_long$car_color[i] = NA
-  }
-}
-
-#driver preferences
-for(i in 1:nrow(month_long)){
-  if(month_long$car_stopped[i] == 1){
-    month_long$prefs[i] = sample(c("hungry", "thirsty", "both"), size = 1,
-                                 prob = c(0.2, 0.5, 0.3))
-  }
-  else{
-    month_long$prefs[i] = NA
   }
 }
 
@@ -86,14 +75,71 @@ for(i in 1:nrow(month_long)){
 #driver coupon
 for(i in 1:nrow(month_long)){
   if(month_long$car_stopped[i] == 1){
-    month_long$coupon[i] = rbernoulli(1, 0.2)
+    month_long$coupon[i] = as.integer(rbernoulli(1, 0.2))
   }
   else{
     month_long$coupon[i] = NA
   }
 }
 
+#driver preferences
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$prefs[i] = sample(c("hungry", "thirsty", "both"), size = 1,
+                                 prob = c(0.2, 0.5, 0.3))
+    month_long$lemon_bar[i] = 0
+    month_long$lemonade[i] = 0
+  }
+  else{
+    month_long$prefs[i] = NA
+    month_long$lemon_bar[i] = NA
+    month_long$lemonade[i] = NA
+  }
+}
 
+#baseline driver purchases
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    if(month_long$prefs[i] == "hungry"){
+      month_long$lemon_bar[i] = month_long$lemon_bar[i] + rpois(1, 1)
+    }
+    else if(month_long$prefs[i] == "thirsty"){
+      month_long$lemonade[i] = month_long$lemonade[i] + rpois(1, 2)
+    }
+    else{
+      month_long$lemon_bar[i] = month_long$lemon_bar[i] + rpois(1, 1)
+      month_long$lemonade[i] = month_long$lemonade[i] + rpois(1, 2)
+    }
+  }
+}
 
+#specific driver preferences
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    if(month_long$gender[i] == "M"){
+      month_long$lemon_bar[i] = month_long$lemon_bar[i] + as.integer(rbernoulli(1, 0.2))
+    }
+    if(month_long$day_of_week[i] %in% c("Mo", "Tu", "We", "Th")){
+      month_long$lemonade[i] == month_long$lemonade[i] + rpois(1, 0.5)
+    }
+    month_long$lemonade[i] =  month_long$lemonade[i] + rpois(1, month_long$age[i]/100)
+    month_long$lemon_bar[i] = month_long$lemon_bar[i] + rpois(1, month_long$age[i]/100)
+  }
+}
 
+#driver spending
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1){
+    month_long$spending[i] = month_long$lemonade[i] + month_long$lemon_bar[i] * 2
+  }
+  else{
+    month_long$spending[i] = NA
+  }
+}
 
+#driver coupon application
+for(i in 1:nrow(month_long)){
+  if(month_long$car_stopped[i] == 1 & month_long$coupon[i] == 1 & month_long$spending[i] > 10){
+    month_long$spending[i] = month_long$spending[i] * 0.85
+  }
+}
